cb5c76f505d674ebe14d6b4ae43a62c95be24014
Alhafat <vladimir-lukin@inbox.ru>
2023-01-19 15:26:48 +0300

    task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -5,30 +5,16 @@
 // 7 -> да
 // 1 -> нет
 
-Console.Write("Введите порядковый номер дня недели: ");
-int x=int.Parse(Console.ReadLine());
+Console.Clear();
 
 while (true)
 {
-    if ((x<0)||(x>7));
+    Console.Write("Введите порядковый номер дня недели: ");
+    int x=int.Parse(Console.ReadLine());
+
+    if ((x<0)^(x>7))
     {
         Console.Write("Такого дня недели не существует");
         continue;
     }
-    // else
-    // {
-    //     if (x==1) Console.Write("Понедельник");
-
-    //     if (x==2) Console.Write("Вторник");
-
-    //     if (x==3) Console.Write("Среда");
-
-    //     if (x==4) Console.Write("Четверг");
-
-    //     if (x==5) Console.Write("Пятница");
-
-    //     if (x==6) Console.Write("Суббота");
-
-    //     if (x==7) Console.Write("Воскресенье");
-    // }
 }
