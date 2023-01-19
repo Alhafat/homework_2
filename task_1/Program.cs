7ae14815b5d4fc38143bd2b0e95b6f4096b7b305
Alhafat <vladimir-lukin@inbox.ru>
2023-01-19 16:34:41 +0300

    task_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -7,20 +7,37 @@
 
 Console.Clear();
 
-Console.Write("Введите любое трехзначное число число: ");
-int number=int.Parse(Console.ReadLine()!);
-
 int count=0;
 int result=0;
 
-while (count<2)
+while (true)
 {
-    result=number/10%10;
-    count++;
-count++;  
-break;
+    Console.Write("Введите трехзначное число: ");
+    int number=int.Parse(Console.ReadLine()!);
+    if ((number<100)||(number>999))
+    {
+        Console.WriteLine($"Неверно выполнено условие!!! Попробуйте еще раз, пожалуйста!!!");
+        continue;
+    }
+    else
+    {
+        // //вариант 1
+        // while (count<2)
+        // {
+        //     result=number/10%10;
+        //     count++;
+        // count++;  
+        // break;
+        // }
+        // Console.WriteLine($"Второй цифрой введенного числа {number} является {result}");
+
+        // //Вариант 2
+        // result=number%100/10;
+        // Console.WriteLine($"Второй цифрой введенного числа {number} является {result}");
+        // break;
+
+        // //Вариант 3
+        Console.WriteLine($"Второй цифрой введенного числа {number} является {number%100/10}");
+        break;
+    }
 }
-
-Console.WriteLine($"Второй цифрой введенного числа {number} является {result}");
-
-// Console.WriteLine($"Второй цифрой введенного числа {number} является {number%100/10}");
